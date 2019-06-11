3421cf2810f5b95c6cdf0dcdec11057bdb2047e1
fummicc1 <fumiya.tennis1@gmail.com>
2019-06-11 19:28:57 +0900

    Initial Commit

---
+++ 20190612_checkwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20190612_checkwork
+//
+//  Created by Fumiya Tanaka on 2019/06/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
